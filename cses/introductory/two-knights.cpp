8597bc601d95f4b4e300da5a1a7089a1c9ab9e7a
Dillon Wu <dillonwu999@gmail.com>
2020-12-28 18:35:08 -0500

    two knights

---
+++ cses/introductory/two-knights.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(a, b) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b; i >= 0; i--)
+
+using namespace std;
+ul m = 1e9+7;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+
+int main() {
+	ul n, x, r, c;
+
+	// 2 + 3 + 3 + 4 for all corners
+	cin >> n;
+	rloop(1, n+1, i) {
+		if (i == 3) {print(28); continue;}
+		if (i == 4) {print(96); continue;}
+		x = i * i;
+		r = x * (x-1) / 2;
+		if (i >= 5) {
+			r = r - (2 * 2 * (i-2) + 2); // 2 columns, n-2 
+			r = r - (2 * 3 * (i-2) + 2);
+			r = r - ((i-4) * 4 * (i-2) + 2 * (i-4));
+		}
+		print(r);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
