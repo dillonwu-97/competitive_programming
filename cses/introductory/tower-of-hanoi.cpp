714691d6e8033af6a68b0b53e409769e8497690a
Dillon Wu <dillonwu999@gmail.com>
2021-01-26 17:48:16 -0500

    tower of hanoi

---
+++ cses/introductory/tower-of-hanoi.cpp
@@ -0,0 +1,147 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(a, b) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+ul m = 1e9+7;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+
+ul exp(ul a, ul b) {
+	ul ret = 1;
+	while(b != 0) {
+		if (b%2 == 1) { ret *= a % m; }
+		b /= 2;
+		a *= a % m;
+	}
+	return ret;
+}
+
+void solve(ul n, ul from, ul to, ul aux) {
+	if (n == 1) {
+		print(from, to);
+	} else {
+		solve(n-1, from, aux, to);
+		print(from, to);
+		solve(n-1, aux, to, from);
+	}
+}
+
+int main() {
+	ul n;
+	cin >> n;
+	print(exp(2, n) -1);
+	solve(n, 1, 3, 2);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
