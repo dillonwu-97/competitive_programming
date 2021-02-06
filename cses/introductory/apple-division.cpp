f22116c334e7dd380fe59cde63291c6802d05d12
Dillon Wu <dillonwu999@gmail.com>
2021-02-06 14:11:20 -0500

    apple division recursion

---
+++ cses/introductory/apple-division.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(a, b) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+ul m = 1e9+7;
+const ul INF = 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+// try using bitsets
+// similar problem to word combinations
+// create combinations of all bit possibilities is also possible
+// all bitset combinations for example
+ul best = INF;
+ul total;
+
+void solve(ul current, int index, const vector<ul> &v) {
+	if (index == v.size()) {
+		current = abs((total - current) - (current));
+		if (current < best) {
+			best = current;
+		}
+	} else {
+		solve (current + v[index], index+1, v);
+		solve (current, index+1, v);
+	}
+
+}
+
+// beware of casting errors using accumulate function
+int main() {
+	ul n, x;
+	cin >> n;
+	vector<ul> v;
+	forward(n, i) {
+		cin >> x;
+		v.push_back(x);
+	}
+	total = 0;
+	forward(v.size(), i) {
+		total+= v[i];
+	}
+	solve(0, 0, v);
+	print(best);
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
