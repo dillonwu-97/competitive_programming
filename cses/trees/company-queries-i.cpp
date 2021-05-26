648acd7a3902db54830916d90e2717131d89e053
Dillon Wu <dillonwu999@gmail.com>
2021-05-26 16:10:05 -0400

    company queries binary lifting

---
+++ cses/trees/company-queries-i.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(b, a) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul m = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+
+// template<typename T, typename T2> void printM(unordered_map<T, T2> d) {
+// 	for (auto it = d.begin(); it != d.end(); it++) {
+// 		cout << it->first << " " << it->second << endl;
+// 	}
+// }
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+
+ul table[200001][32];
+
+int main() {
+	ul n, x, r, c;
+	cin >> n >> x;
+
+	// clear bits
+	forward(5001, i) {
+		forward(32, j) {
+			table[i][j] = 0;
+		}
+	}
+
+	table[1][0] = 1; // root
+	rloop(2, n+1, i) {
+		cin >> r;
+		table[i][0] = i; // 0 away from self
+		table[i][1] = r; // one away from self is the root
+	}
+
+	for (ul j = 2; j < 32; j++) {
+		for (ul i = 2; i <= n; i++) {
+			table[i][j] = table [table[i][j-1]] [j-1];
+		}
+	}
+
+	// printAA(table, 10, 10);
+	ul ret;
+	while (x--) {
+		cin >> r >> c;
+		for (int i = 32; i > 0; i--) {
+			if (r == 0) {
+				break;
+			}
+			if (c & (1 << (i - 1 ))) {
+				// print("i is ", i);
+				r = table[r][i];
+			}
+		}
+		if (r == 0) {print(-1); continue;}
+		print(r);
+
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
