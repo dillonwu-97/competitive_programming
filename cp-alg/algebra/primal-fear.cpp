18c0eb9a6de0e528d5a38761a9f76eac1df4c81b
Dillon Wu <dillonw@andrew.cmu.edu>
2022-01-18 18:35:29 -0500

    spoj primal fear

---
+++ cp-alg/algebra/primal-fear.cpp
@@ -0,0 +1,189 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(b, a) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul m = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void print(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+
+// template<typename T, typename T2> void printM(unordered_map<T, T2> d) {
+// 	for (auto it = d.begin(); it != d.end(); it++) {
+// 		cout << it->first << " " << it->second << endl;
+// 	}
+// }
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+
+ul cap = 1e6;
+vector<char> nums(cap, 0);
+vector<ul> primes;
+ul temp;
+void seive() {
+    nums[0] = nums[1] = 1;
+    for (ul i = 2; i * i < cap; i++){
+        if (nums[i] == 0) {
+            for (ul j = i * i; j < cap; j+= i) {
+                if (nums[j] == 0) {
+                    nums[j] = 1;
+                }
+            }
+        } 
+    }
+}
+
+ul get_floor(ul num) {
+    ul start = 100000;
+    for (ul i = 0; i < 6; i++) {
+        if (num > start) {
+            break;
+        }
+        start /= 10;
+    }
+    return start/10;
+}
+
+ul get_next_num(ul num) {
+    ul start = 10 * 10;
+    for (ul i = 2; i < 6; i++) {
+        if (num < start) {
+            break;
+        }
+        start *= 10;
+    }
+    return num % (start / 10);
+}
+
+char check(ul n) {
+    // print("n is ", n);
+    if (nums[n] == 0) {
+        ul next_num = get_next_num (n);
+        ul floor_num = get_floor(n);
+        // print(n, next_num, floor_num);
+        if (next_num < floor_num || nums[n] == 1){
+            return 1;
+        } else {
+            nums[n] = check ( next_num );
+        }
+    }
+    return nums[n];
+}
+
+void find_primes() {
+    for (ul i = cap-1; i >= 2; i--) {
+        if (nums[i] == 0) {
+            nums[i] = check(i);
+        }
+        if (nums[i] == 2) {
+            primes.push_back(i);
+        }
+    }
+}
+
+int main() {
+	ul n, x, r, c;
+    seive();
+    nums[2] = nums[3] = nums[5] = nums[7] = 2; // used in find_primes
+    find_primes();
+    cin >> n;
+    reverse(primes.begin(), primes.end());
+    vector<ul>::iterator it;
+    while (n--) {
+        cin >> x;
+        it = upper_bound(primes.begin(), primes.end(), x);
+        print(it - primes.begin());
+
+    }
+    // for (ul i = 0; i < primes.size(); i++) {
+    //     print(primes[i]);
+    // }
+    return 0;
+
+
+}
+
+
