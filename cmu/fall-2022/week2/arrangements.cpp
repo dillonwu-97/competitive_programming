2ac641f6110fd01bae6a83a2a8bd9f1eedb2b7d9
Dillon Wu <dillonw@andrew.cmu.edu>
2022-11-11 01:42:03 -0500

    arrangements finished

---
+++ cmu/fall-2022/week2/arrangements.cpp
@@ -0,0 +1,168 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(b, a) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+//const ul m = 1e9+7;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void print(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+
+// template<typename T, typename T2> void printM(unordered_map<T, T2> d) {
+// 	for (auto it = d.begin(); it != d.end(); it++) {
+// 		cout << it->first << " " << it->second << endl;
+// 	}
+// }
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+int main() {
+    ul n, m, k;
+    cin >> n >> m >> k;
+    char grid[2000][2000];
+    char c;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> c;
+            grid[i][j] = c;
+            //cout << grid[i][j] << " ";
+        }
+        //cout << endl;
+    }
+
+    ul ret = 0;
+
+    // Handlling rows
+    int lb = 0, rb = 0, j = 0;
+    for (int i = 0; i < n; i++) {
+        //print("i is: ", i);
+        lb = 0; rb = 0; j = 0;
+        while (j < m) {
+            while (grid[i][j] == '.' && j < m) {
+                j ++;
+                rb ++;
+            }
+            if (rb - lb >= k) {
+                ret += rb - lb -k + 1;
+            }
+            while (grid[i][j] == '*' && j < m) {
+                j ++;
+            }
+            lb = j;
+            rb = lb;
+            
+        }
+    }
+    if (k == 1) {
+        print(ret);
+        return 0 ;
+    }
+
+    int i = 0;
+    lb = 0; rb = 0;
+    // Handling columns
+    for (int j = 0; j < m; j++) {
+        //print("j is: ", j);
+        lb = 0; rb = 0; i = 0;
+        while (i < n) {
+            while (grid[i][j] == '.' && i < n) {
+                i++;
+                rb++;
+            }
+            if (rb - lb >= k) {
+                ret += rb - lb - k + 1;
+            }
+            while (grid[i][j] == '*' && i < n) {
+                i++;
+            }
+            lb = i;
+            rb = lb;
+        }
+    }
+
+    print(ret);
+
+}
+
+
