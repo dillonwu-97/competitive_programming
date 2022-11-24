69d53fcb54d49fdaac171b001b0be3a36b6fd8f3
Dillon Wu <dillonw@andrew.cmu.edu>
2022-11-23 23:12:50 -0500

    finished lost graph dfs

---
+++ cmu/fall-2022/week5/lost-graph.cpp
@@ -0,0 +1,129 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <stack>
+
+#define ul unsigned long long
+#define sl signed long long
+# define pii pair<int, int>
+#define f first
+#define s second
+
+#define aloop(b, a) for (auto &a: b)
+#define rloop(a, b, i) for (int i = a; i < b; i++)
+#define forward(a, i) for (int i = 0; i < a; i++)
+#define backward(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul m = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void print(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void print(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void print(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void print(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void printV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printQ(queue<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+template<typename T> void printS(set<T> s) {
+	typename set<T>::iterator it;
+	for (T const&i: s) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+
+// template<typename T, typename T2> void printM(unordered_map<T, T2> d) {
+// 	for (auto it = d.begin(); it != d.end(); it++) {
+// 		cout << it->first << " " << it->second << endl;
+// 	}
+// }
+
+template<typename T> void printA(const T (&a), int N) {
+	forward(N, i) {
+		cout << a[i] << " ";
+	}
+	cout << endl;
+}
+
+// this ONLY works if we know the size of N and M at compile time
+// also known as stack living arrays
+template<typename T> void printAA(const T (&a), int N, int M) {
+	forward(N, i) {
+		forward(M, j) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+}
+
+/********************* Common functions *********************/
+
+template <typename T> T abs(T n) {
+	return n < 0? n * -1 : n; 
+}
+
+bool p2(ul x)
+{
+    return (x & (x - 1)) == 0;
+}
+
+/********************* Main method *********************/
+int main() {
+    int n, x, r, c, prev, curr;
+    string s;
+    cin >> n;
+    stack<int> q;
+    pair<int, int> p;
+    vector<pair<int,int>> ret;
+    q.push(-1);
+    while (cin >> s) {
+        cin >> curr;
+        if (s == "in") {
+            prev = q.top();
+            q.push(curr);
+            if (prev != -1) {
+                ret.push_back(make_pair(curr, prev));
+            }
+            //print("Front is: ", curr, prev);
+        } else {
+            q.pop();
+        }
+
+    }
+    for (auto i: ret) {
+        cout << i.f << " " << i.s << endl;
+    }
+}
+
+
