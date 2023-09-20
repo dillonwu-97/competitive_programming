126c19f0d5c61d3ed13b98a574cabd77272d054c
Dillon Wu <dillonw@andrew.cmu.edu>
2023-09-20 17:01:57 -0400

    more number theory easy

---
+++ codeforces/number_theory/forbidden-integer.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <stack>
+
+#define ul unsigned long long
+#define sl signed long long
+#define pii pair<sl, sl>
+#define f first
+#define s second
+#define v vector
+#define pb push_back
+
+#define au(a, b) for (auto &a: b)
+#define f2(a, b, i) for (int i = a; i < b; i++)
+#define f1(a, i) for (int i = 0; i < a; i++)
+#define b1(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul MAX = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void p(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void p(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void p(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void p(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void pV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+/********************* Main method *********************/
+// special cases problem seems like
+int main() {
+    sl t, n, k, x;
+    cin >> t;
+    while (t--) {
+        cin >> n >> k >> x;
+        if (n % 2 == 1 && k == 2 && x == 1) {
+            p("NO");
+        } else if (k == 1) {
+            p("NO");
+        } else {
+            p("YES");
+            if (x == 1) {
+                p(n/2);
+                if ( n % 2 == 1 ) {
+                    cout << 3 << " ";
+                    n -= 3;
+                }
+                for (sl i = 0; i < n/2; i++) {
+                    cout << 2 << " ";
+                }
+                cout << endl;
+            } else {
+                cout << n << endl;
+                for (sl i = 0; i < n; i++) {
+                    cout << 1 << " ";
+                }
+                cout << endl;
+            }
+        }
+    }
+}
+
+
