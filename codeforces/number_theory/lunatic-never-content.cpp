274dad665dc9a669c696a7a6a07b13fd00137d7d
Dillon Wu <dillonw@andrew.cmu.edu>
2023-09-20 21:17:34 -0400

    more gcd

---
+++ codeforces/number_theory/lunatic-never-content.cpp
@@ -0,0 +1,83 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <numeric>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <stack>
+
+#define ul unsigned long long
+#define sl signed long long
+#define pii pair<sl, sl>
+#define f first
+#define s second
+#define v vector
+#define pb push_back
+
+#define au(a, b) for (auto &a: b)
+#define f2(a, b, i) for (int i = a; i < b; i++)
+#define f1(a, i) for (int i = 0; i < a; i++)
+#define b1(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul MAX = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void p(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void p(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void p(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void p(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void pV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+/********************* Main method *********************/
+int main() {
+    sl t, n, x, ret;
+    sl left, right;
+    cin >> t;
+    while (t--) {
+        cin >> n;
+        v<sl> v;
+        ret = 0;
+        while (n--) {
+            cin >> x;
+            v.pb(x);
+        }
+        left = 0; right = v.size()-1;
+        while (left < right) {
+            if (v[left] != v[right]) {
+                if (ret == 0) {
+                    ret = abs( v[left] - v[right] );
+                }
+                ret = gcd( abs( v[left] - v[right] ) ,ret);
+            }
+            left++;
+            right--;
+        }
+        p (ret);
+
+    }
+}
+
+
