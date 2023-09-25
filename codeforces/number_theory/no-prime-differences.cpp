c5964c9ba3efdfee74556a82497a04c304051abb
Dillon Wu <dillonw@andrew.cmu.edu>
2023-09-24 21:24:07 -0400

    more number theory

---
+++ codeforces/number_theory/no-prime-differences.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <stack>
+
+#define ul unsigned long long
+#define sl signed long long
+#define pii pair<sl, sl>
+#define f first
+#define s second
+#define v vector
+#define pb push_back
+
+#define au(a, b) for (auto &a: b)
+#define f2(a, b, i) for (int i = a; i < b; i++)
+#define f1(a, i) for (int i = 0; i < a; i++)
+#define b1(b, i) for (int i = b-1; i >= 0; i--)
+
+using namespace std;
+const ul MAX = 1e9+7;
+const ul INF= 1e18;
+
+/********************* Print statements *********************/
+template <typename T> void p(T s) {
+	cout << s << endl;
+}
+
+template <typename T, typename T2> void p(T s, T2 s2) {
+	cout << s << " " << s2 << endl;
+}
+
+template <typename T, typename T2, typename T3> void p(T s, T2 s2, T3 s3) {
+	cout << s << " " << s2 << " " << s3 << endl;
+}
+
+template <typename T, typename T2, typename T3, typename T4> void p(T s, T2 s2, T3 s3, T4 s4) {
+	cout << s << " " << s2 << " " << s3 << " " << s4 << endl;
+}
+
+template<typename T> void pV(vector<T> v) {
+	for (const auto & i: v) {
+		cout << i << " ";
+	}
+	cout << endl;
+}
+
+/********************* Main method *********************/
+int main() {
+    sl t, n, m;
+    cin >> t;
+    while (t--) {
+        cin >> n >> m;
+        v<v<sl>> g(n, v<sl>(m,0));
+        // do a transpose instead
+        for (sl i = 0; i < n; i++) {
+            for (sl j = 0; j < m; j++) {
+                if (n == 4) {
+                    g[i][j] = (j) * n + 1 + i;
+                } else {
+                    g[i][j] = j+1 + i * m;
+                }
+            }
+        }
+        if (n == 4) {
+            f1(n,i) {
+                pV(g[i]);
+            }
+        } else {
+            f1(n, i) {
+                if (i % 2 == 0) {
+                    pV(g[i/2]);
+                } else {
+                    pV(g[(n+1)/2 + i/2]);
+                }
+            }
+        }
+        cout << endl;
+    }
+}
+
+
